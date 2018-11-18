c44d1caa32851b3b87ab2b19cc2b408c56601514
Eric Stimpson <estimpson@fore-thought.com>
2018-11-18 12:23:29 -0500

    Update Global.asax.cs
    Retrieve user from cookie.

--- EmpirePortal/EmpirePortal.Mvc/Global.asax.cs
+++ EmpirePortal/EmpirePortal.Mvc/Global.asax.cs
@@ -1,7 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 using EmpirePortal.Mvc.App_Start;
+using FxWeb.Mvc.Infrastructure.Security.Authorization;
+using Newtonsoft.Json;
 
 namespace EmpirePortal.Mvc
 {
@@ -15,6 +20,26 @@ namespace EmpirePortal.Mvc
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ModelBinders.Binders.DefaultBinder = new DevExpress.Web.Mvc.DevExpressEditorsBinder();
+        }
+
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            var authCookie = Request.Cookies["User"];
+            if (authCookie == null) return;
+            var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            if (authTicket == null) return;
+            var serializeModel = JsonConvert.DeserializeObject<FxSerializeUserModel>(authTicket.UserData);
+
+            var principal = new FxPrincipal(authTicket.Name)
+            {
+                UserId = serializeModel.UserId,
+                FirstName = serializeModel.FirstName,
+                LastName = serializeModel.LastName,
+                SecurityGuid = serializeModel.SecurityGuid,
+                Roles = serializeModel.RoleName?.ToArray()
+            };
+
+            HttpContext.Current.User = principal;
         }
     }
 }
